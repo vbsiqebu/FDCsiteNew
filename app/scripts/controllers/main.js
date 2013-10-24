c0e9b453e4cf169002e2415caf9d3d2389333f6a
vbisqebu <hatch.josiah@gmail.com>
2013-10-24 11:33:19 +0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('FDCsiteNewApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
