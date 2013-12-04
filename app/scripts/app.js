b3cd345b77b8260d16722209c352c02d1d49a74a
vbisqebu <hatch.josiah@gmail.com>
2013-12-04 22:01:41 +0800

    Added Profiles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,38 @@
 'use strict';
 
-angular.module('FDCsiteNewApp', [])
+angular.module('FDCsiteNewApp', ['ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/aboutus', {
+        templateUrl: 'views/aboutus.html',
+        controller: 'MainCtrl'
+      })
+      .when('/mission', {
+        templateUrl: 'views/mission.html',
+        controller: 'MainCtrl'
+      })
+      .when('/ourteam', {
+        templateUrl: 'views/ourteam.html',
+        controller: 'MainCtrl'
+      })
+      .when('/services', {
+        templateUrl: 'views/services.html',
+        controller: 'MainCtrl'
+      })
+      .when('/articles', {
+        templateUrl: 'views/articles.html',
+        controller: 'MainCtrl'
+      })
+      .when('/career', {
+        templateUrl: 'views/career.html',
+        controller: 'MainCtrl'
+      })
+      .when('/contactus', {
+        templateUrl: 'views/contactus.html',
         controller: 'MainCtrl'
       })
       .otherwise({
